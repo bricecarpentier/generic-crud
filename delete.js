0f405797e272fed6526b3a7eb8d608c3a7f1ccbc
Brice Carpentier <brice@daknet.org>
2010-11-11 11:57:13 +0100

    changed API (again) : controllers that need to lookup an object take a query parameter, which is a method that get passed an id and a callback as a parameter

--- delete.js
+++ delete.js
@@ -1,10 +1,10 @@
 var errors = require('./errors')
 
-var delete_fn = function(model, post_delete_redirect) {
+var delete_fn = function(query, post_delete_redirect) {
     
     return function(req, res) {
         if (req.method === 'DELETE') {
-            model.findById(req.params.id, function(object) {
+            query(req.params.id, function(object) {
                 object.remove()
                 req.flash('info', 'The object was deleted')
                 res.redirect(post_delete_redirect)
--- detail.js
+++ detail.js
@@ -1,7 +1,7 @@
-var detail = function(model, template, extra_context) {
+var detail = function(query, template, extra_context) {
     
     return function(req, res) {
-        model.findById(req.params.id, function(object) {
+        query(req.params.id, function(object) {
             
             var locals = {}
             locals.object = object
